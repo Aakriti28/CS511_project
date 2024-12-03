f9cbfd91c53125cb1caf2af3c59eaabcaca2e2a2
Akshat Sharma <akshat.iitkanpur@gmail.com>
2024-12-02 22:45:16 -0600

    cpu exp plot subset

--- vs_cpu_plots/cpu_plotter.py
+++ vs_cpu_plots/cpu_plotter.py
@@ -7,8 +7,17 @@ logger = logging.getLogger(__name__)
 # set format including level
 logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.INFO)
 
+operation_to_name = {
+    'load_dataset': 'Load Dataset',
+    'locate_null_values': 'Locate Null Values',
+    'drop_duplicates': 'Drop Duplicates',
+    'join': 'Join'
+}
+
 if __name__ == '__main__':
     num_cpus = [1,2,4,8]
+    required_methods = ['load_dataset', 'locate_null_values', 'drop_duplicates', 'join']
+    
     cpu_to_df = {}
     for num_cpu in num_cpus:
         csv_files = glob.glob(f'../results_cpu/modin_dask_mem80_cpu{num_cpu}/*.csv')
@@ -26,19 +35,19 @@ if __name__ == '__main__':
     num_operations = len(cpu_to_df[1])
     logger.info(f"{num_operations = }")
     
-    # plot 14 operations
-    # each plot has time for that op for 1,2,4,8 cpus
-    fig, axs = plt.subplots(2, 7, figsize=(20, 10))
-    fig.suptitle('Time Taken for each DF Operation for Multiple CPUs', fontsize=16, fontweight='bold')
-    sorted_methods = sorted(cpu_to_df[1]['method'])
-    for i, operation in enumerate(sorted_methods):
-        axs[i//7, i%7].set_title(operation)
-        axs[i//7, i%7].set_xlabel('Number of CPUs')
-        axs[i//7, i%7].set_ylabel('Time (s)')
-        axs[i//7, i%7].grid(True)
+    # plot only the required methods in a 2x2 figure
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+    fig.suptitle('Time Taken For Select DF Operations On Multiple CPUs On The Loan Dataset', fontsize=16, fontweight='bold')
+    for i, operation in enumerate(required_methods):
+        ax = axs[i // 2, i % 2]
+        ax.set_title(operation_to_name.get(operation, operation))
+        ax.set_xlabel('Number of CPUs')
+        ax.set_ylabel('Time (s)')
+        ax.grid(True)
         times = [cpu_to_df[num_cpu][cpu_to_df[num_cpu]['method'] == operation]['time'].values[0] for num_cpu in num_cpus]
-        axs[i//7, i%7].bar(num_cpus, times)
-        axs[i//7, i%7].set_xticks(num_cpus)
+        ax.bar(range(len(num_cpus)), times)
+        ax.set_xticks(range(len(num_cpus)))
+        ax.set_xticklabels(num_cpus)
         
-    plt.tight_layout()
-    plt.savefig('./plots/cpu_plots.png', dpi=300)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig('./plots/cpu_plots_selected_methods.png', dpi=300)
